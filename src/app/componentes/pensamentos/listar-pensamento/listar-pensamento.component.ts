b8163821babfed15cd1ec42832ef778be4fa00c4
Cleber Simm <csimm@casan.com.br>
2023-05-11 08:11:48 -0300

    init services with http

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,18 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Aaaaangularrrrr  :)',
-      autoria: 'Eu',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Angular 2',
-      autoria: 'Eu',
-      modelo: 'modelo1'
-    }
-  ]
+  listaPensamentos: Pensamento[] = []
 
   constructor() { }
 
