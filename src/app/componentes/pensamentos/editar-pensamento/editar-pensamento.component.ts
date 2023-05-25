e512045fb5acb49d38f99fce82abf8625ad09ff4
Cleber Simm <csimm@casan.com.br>
2023-05-25 07:53:26 -0300

    finished fombuilder started edit with formbuilder

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -9,29 +10,38 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./editar-pensamento.component.css']
 })
 export class EditarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  formulario!: FormGroup
+
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento)=>{
-      this.pensamento = pensamento
+      this.formulario = this.formBuilder.group({
+        conteudo: [pensamento.conteudo,Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\s(.|\s)*/)
+        ])],
+        autoria: [pensamento.autoria, Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])],
+        modelo: pensamento.modelo
+      })
     })
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(()=>{
-      this.router.navigate(['/listarPensamento'])
-    })
+    if(this.formulario.valid){
+      this.service.editar(this.formulario.value).subscribe(()=>{
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar() {
