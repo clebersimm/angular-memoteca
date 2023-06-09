f55e77ca5c704e913aceda7580e0b011f7ac1798
Cleber Simm <clebersimm@gmail.com>
2023-06-09 08:27:43 -0300

    favoritos

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -12,9 +12,17 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(pagina: number): Observable<Pensamento[]> {
+  listar(pagina: number, filtro: string, favoritos: boolean): Observable<Pensamento[]> {
     const itensPorPagina = 6
-    let params = new HttpParams().set('_page', pagina).set('_limite', itensPorPagina)
+    let params = new HttpParams()
+      .set('_page', pagina)
+      .set('_limite', itensPorPagina)
+      if (filtro.trim().length > 2) {
+        params = params.set('q', filtro)
+      }
+      if(favoritos){
+        params = params.set("favorito",true)
+      }
     return this.http.get<Pensamento[]>(this.API, { params })
     //return this.http.get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)
   }
@@ -26,6 +34,11 @@ export class PensamentoService {
   editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`
     return this.http.put<Pensamento>(url, pensamento)
+  }
+
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito
+    return this.editar(pensamento)
   }
 
   excluir(id: number): Observable<Pensamento> {
